18fdc9908ab0eb6e3eb9f39df5eb1304b79a6243
vagrant <vagrant@homestead>
2019-09-09 03:24:54 +0000

    add game model

---
+++ database/migrations/2019_09_09_112346_create_games_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGamesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->integer('match_num')->nullable();
+            $table->date('game_date')->nullable();
+            $table->integer('status')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('games');
+    }
+}
