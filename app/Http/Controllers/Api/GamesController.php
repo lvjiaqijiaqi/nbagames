0bb90ab3f4d625677b9e3ca1721022c49cd63bef
vagrant <vagrant@homestead>
2019-10-31 05:16:51 +0000

    update

--- app/Http/Controllers/Api/GamesController.php
+++ app/Http/Controllers/Api/GamesController.php
@@ -14,8 +14,8 @@ class GamesController extends Controller
 {
     public function index()
     {
-    	$game = Game::where('status', '!=', '4')->first();
-    	$players = GamePlayerData::where('game_id', '=', $game->id)->get();
+    	$game = Game::where('status', '!=', '3')->first();
+    	$players = GamePlayerData::where('game_id', '=', $game->id)->orderBy('id','desc')->get();
     	$game->gamePlayers = $players;
 		  $game->room = Room::first();
 		  $game->play = Play::where(array('game_id' => $game->id , 'room_id' => $game->room->id , 'user_id' => $this->user()->id))->first();
