7d89f82d6800bedf25b4477e41ab63e31d3bbb3f
vagrant <vagrant@homestead>
2019-11-01 06:20:01 +0000

    兜底

--- app/Http/Controllers/Api/GamesController.php
+++ app/Http/Controllers/Api/GamesController.php
@@ -15,10 +15,14 @@ class GamesController extends Controller
     public function index()
     {
     	$game = Game::where('status', '!=', '3')->orderBy('id','desc')->first();
-    	$players = GamePlayerData::where('game_id', '=', $game->id)->get();
-    	$game->gamePlayers = $players;
-		  $game->room = Room::first();
-		  $game->play = Play::where(array('game_id' => $game->id , 'room_id' => $game->room->id , 'user_id' => $this->user()->id))->first();
-    	return $this->response->item($game, new GameTransformer());
+      if ($game) {
+          $players = GamePlayerData::where('game_id', '=', $game->id)->get();
+          $game->gamePlayers = $players;
+          $game->room = Room::first();
+          $game->play = Play::where(array('game_id' => $game->id , 'room_id' => $game->room->id , 'user_id' => $this->user()->id))->first();
+          return $this->response->item($game, new GameTransformer());
+      }else{
+          return $this->response->error('没有找到游戏', 404);
+      }
     }
 }
